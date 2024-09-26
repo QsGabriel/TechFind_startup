16d62e4b8466cfbf02a2ae4dd4699d3b6c32c298
Gabriel Queiroz <142359384+QsGabriel@users.noreply.github.com>
2024-09-26 00:32:55 -0300

    Descrição do que foi alterado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -34,6 +34,30 @@ $(document).ready(function () {
     $(navItems[activeSectionIndex]).addClass("active");
   });
 
+  $(document).ready(function(){
+    $('.carousel').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+    });
+  });
+
   ScrollReveal().reveal("#home", {
     origin: "top",
     duration: 2000,
@@ -58,3 +82,12 @@ $(document).ready(function () {
     distance: "40%",
   });
 });
+
+  ScrollReveal().reveal("#aboutus", {
+  origin: "left",
+  duration: 2000,
+  distance: "40%",
+  });
+
+  
+  
